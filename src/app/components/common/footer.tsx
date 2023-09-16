7af990f76b5131fda75c87b54b96c52b8b2592a3
RanaAliAbid <72215018+RanaAliAbid@users.noreply.github.com>
2023-09-16 14:23:46 +0400

    link changes

--- src/app/components/common/footer.tsx
+++ src/app/components/common/footer.tsx
@@ -279,7 +279,7 @@ export default function Footer() {
                       Privacy & Cookie{" "}
                     </a>
                   </li>
-                  <li>
+                  {/* <li>
                     <a
                       href="https://docs.lfi.io/legal-documents/minting-terms-and-condition"
                       target="_blank"
@@ -296,7 +296,7 @@ export default function Footer() {
                       {" "}
                       cLFi Terms{" "}
                     </a>
-                  </li>
+                  </li> */}
                   <li>
                     <a
                       href="https://docs.lfi.io/legal-documents/disclaimer"
